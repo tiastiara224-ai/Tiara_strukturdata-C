fc013a7fa04f6150798522e1e411ead44704e98e
Tiara_Alprog-C <tiastiara224@gmail.com>
2026-04-19 19:58:45 +0800

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,11 @@
+#tugas 
+#jika nama yang diinputkan benar, maka akaan memunculkan nim kalian 
+#jika nama yang diinputkan salah, maka akan memunculkan "nama salah"
+
+nama = input("masukan nama")
+nim = input("25241086")
+if nama == "tiara" :
+    print("masukan nim saya")
+else :
+    print("nama anda salah")
+    print("program selesai")
