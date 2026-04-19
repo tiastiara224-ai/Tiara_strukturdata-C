0acadba417c939877075d3a6e7394c22d48eddef
Tiara_Alprog-C <tiastiara224@gmail.com>
2026-04-19 21:01:18 +0800

    Add files via upload

---
+++ while3.py
@@ -0,0 +1,13 @@
+#tugas 3
+while True:
+    angka =int(input("masukan angka yang anda mau"))
+
+    if angka >= 0:
+        print (f"niali angka:{angka}")
+        print ("tiara cantik")
+
+    else:
+         print("harus positif!")
+         print("silahkan masukan angka yg positif di hlaman berikut")
+
+    print (f"terimakasih anda memasukan angka{angka}")
