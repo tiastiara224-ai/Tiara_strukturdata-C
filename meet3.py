b1a970a224d21907fdc49cf081820f60f7fe5a3e
Tiara_Alprog-C <tiastiara224@gmail.com>
2026-04-19 21:04:43 +0800

    Add files via upload

---
+++ meet3.py
@@ -0,0 +1,11 @@
+# whille loop
+# kodisi
+# aksi
+
+angka =0
+
+while angka < 5:
+    angka += 1
+    print(f"nilai angka:{angka}")
+    print("mencintai seseorang yang tidak mencintai kita sangat sakit tidak ada duanya")
+print("program ke 3 selse pak")
