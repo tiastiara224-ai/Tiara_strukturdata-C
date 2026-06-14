3ab2fd50d0555be3268e2402856209c9d954abcd
Tiara_Alprog-C <tiastiara224@gmail.com>
2026-06-14 16:01:28 +0800

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+#pengenalan list
+
+data1 = 1
+data2 = 2
+data3 = 3
+
+angka = [1, 2, 3]
+print(f"kumpulan data angka :\n{angka}")
+
+string = ["durian", "apple", "orange"]
+print(f"kumpulan data string :\n{string}")
+
+bolean = [True,False,True]
+print(f"kumpulan bolean:\n{bolean}")
+
+cam=[3,9,"durian","True",False]
+print(f"kumpulan data campuran\n{cam}")
+
+#cara alternatif membuat list
+data_range=list(0,10)
+data_list=list(data_range)
+print(f"data_range:\n{data_list}")
+
+#membuat list menggunakan for
